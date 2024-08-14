56ca0b06377ac7195eab5832ce75ceaa9bd4ea84
Joshua-Frost <gky3846@autuni.ac.nz>
2024-08-15 09:16:39 +1200

    Small change to 404 screen to test deployment

--- worksheet4/frontend/src/pages/404.tsx
+++ worksheet4/frontend/src/pages/404.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <div className="container">
-        <h1>This is probably not the page you were expecting... (404)</h1>
+        <h1>This is probably not the page you were expecting... Try somewhere else.. (404)</h1>
       </div>
     );
   }
